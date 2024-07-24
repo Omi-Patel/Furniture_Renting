79935d5bceb11fc15220080ca21cc4d5b775a56b
Parmesh119 <parmeshb90@gmail.com>
2024-07-24 19:15:47 +0530

    updated

--- client/src/components/SignIn.jsx
+++ client/src/components/SignIn.jsx
@@ -100,7 +100,7 @@ export default function SignInThree() {
         <div className="flex items-center justify-center px-4 py-10 sm:px-6 sm:py-16 lg:px-8 lg:py-24">
           <div className="xl:mx-auto xl:w-full xl:max-w-sm 2xl:max-w-md">
             <div className="mb-2 flex justify-center">
-            <img src="src/assets/Signin_SignUp.png" width={100} alt="Furniture Rentals" />
+            <img src="https://i.ibb.co/hHDy6pv/logo.png" width={100} alt="Furniture Rentals" />
             </div>
             <h2 className="text-center text-2xl font-bold leading-tight text-black">
               Sign in to your account
